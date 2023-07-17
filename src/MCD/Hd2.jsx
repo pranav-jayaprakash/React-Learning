c26aec5c1d442e8103b9cefa0fc8c6af06048568
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-17 13:08:36 +0530

    card complete

--- src/MCD/Hd2.jsx
+++ src/MCD/Hd2.jsx
@@ -21,7 +21,22 @@ function Hd2() {
     head:`Score Free Food with McDelivery®`,
     sb:`Earn MyMcDonald’s Rewards points on every McDelivery order only in the McD’s app to redeem for free food. And, yup, you can get ‘em delivered, too. Just order, relax and enjoy—check out how easy ordering delivery is in the app.*`,
     sm:`*At participating McDonald's. Only in the app. App registration required. Must be opted into MyMcDonald's Rewards.`,
-    btn:`Earn Points With McDelivery`}
+    btn:`Earn Points With McDelivery`},
+    {image:`https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1pub_FreeLargeFries_2336x1040.jpg`,
+    head:`Free Large Fries Now. Even More Fries Later.`,
+    sb:`Download the McDonald’s app and join MyMcDonald’s Rewards to get your free large Fries with $1 minimum purchase.* Plus, when you join MyMcDonald’s Rewards, you start earning points on every eligible order—points you can put towards more free food.`,
+    sm:`*Offer valid 1x thru the last day of the month for first time app users at participating McDonald's. May take up to 48 hours to appear in your deals. Must opt in to MyMcDonald’s Rewards. Excludes tax.`,
+    btn:`Get Free Fries in the App`},
+    {image:`https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1PUB_RyC_CAMILO_2336x1040.jpg`,
+    head:`Want to Experience Camilo Live? Vote Today!`,
+    sb:`McDonald’s Ritmo y Color, a celebration of urban music, art and Latino culture, is proud to present an exclusive live concert experience featuring Grammy-nominated artist Camilo. Putting the power in the hands of fans nationwide, fans will be able to bring Camilo live to two cities from a choice of six venues: New York, San Antonio, Dallas, Chicago, San Jose or Miami. Vote now to bring Camilo to your city! Voting is open from June 27 to July 18, 2023.`,
+    sm:``,
+    btn:`Vote Now`},
+    {image:`https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1PUB_Desktop_Deals-v1_1168x520.jpg`,
+    head:`Deals for Days`,
+    sb:`Get exclusive deals on your McDonald’s favorites in the app with contactless Mobile Order & Pay* and convenient Drive Thru or Curbside pickup.`,
+    sm:`*Mobile Order & Pay at participating McDonald’s.`,
+    btn:`Get App Deals`}
     ]
   return (
     <div>
