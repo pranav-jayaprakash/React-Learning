a3b88138c4ccaeae4587bdd284c8f5a8e1450c1e
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-07-17 12:58:31 +0530

    card update

--- src/MCD/Hd2.jsx
+++ src/MCD/Hd2.jsx
@@ -12,6 +12,16 @@ function Hd2() {
     ^Fresh Beef available at most restaurants in contiguous U.S. Not available in Alaska, Hawaii and U.S. Territories.
     +At participating McDonald's. Only in the app. App registration required. Must be opted in to MyMcDonald's Rewards.`,
     btn:'Try the New QPC'},
+    {image:'https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1PUB_ROA_2336x1040v3.jpg',
+    head:'New: Faster Faves, Only in the App',
+    sb:'We now prep when you’re on the way if you choose Curbside, Front Counter—or dine in for Table Service. Just order ahead in the app to save time. Because waiting in line for faves? Not our thing either.*',
+    sm:`*At participating McDonald’s.`,
+    btn:'Order Ahead in the App'},
+    {image:'https://www.mcdonalds.com/content/dam/sites/usa/nfl/publication/1pub_McDeliveryintheApp_Launch_2336x1040_v3.jpg',
+    head:`Score Free Food with McDelivery®`,
+    sb:`Earn MyMcDonald’s Rewards points on every McDelivery order only in the McD’s app to redeem for free food. And, yup, you can get ‘em delivered, too. Just order, relax and enjoy—check out how easy ordering delivery is in the app.*`,
+    sm:`*At participating McDonald's. Only in the app. App registration required. Must be opted into MyMcDonald's Rewards.`,
+    btn:`Earn Points With McDelivery`}
     ]
   return (
     <div>
