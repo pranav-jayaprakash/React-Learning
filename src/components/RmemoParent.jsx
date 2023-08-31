b8a14457c6afb019d1ceadd1648554f62574d124
Mona Lisa <pranavjayaprakashn98@gmail.com>
2023-08-31 14:50:18 +0530

    update

--- src/components/RmemoParent.jsx
+++ src/components/RmemoParent.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function RmemoParent() {
-    
+
+    const [state, setstate] = useState(0)
+    const [state1, setstate1] = useState(0)
+
+
+    const show=()=>{
+        setstate(state+1)
+    }
+
+    const show1=()=>{
+        setstate1(state1+1)
+    }
+
   return (
     <div>
+        <h1>{state}</h1>
+        <h1>{state1}</h1>
+
+        <button onClick={show}>Click</button>
+        <button onClick={show1}>Click2</button>
+
+        
         
     </div>
   )
